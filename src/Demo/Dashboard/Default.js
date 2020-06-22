2203382d3d9010e17fc8429d257f248797a8e246
musamthembu84@gmail.com <Streetwise22@22>
2020-06-22 23:04:06 +0200

    last fix on UI

--- src/Demo/Dashboard/Default.js
+++ src/Demo/Dashboard/Default.js
@@ -186,14 +186,14 @@ class Dashboard extends React.Component {
                             <Card.Body>
                                 <div className="row align-items-center justify-content-center">
                                     <div className="col">
-                                        <h5 className="m-0">Upcoming Event</h5>
+                                        <h5 className="m-0">UJ SRC Elections</h5>
                                     </div>
                                     <div className="col-auto">
-                                        <label className="label theme-bg2 text-white f-14 f-w-400 float-right">34%</label>
+                                        <label className="label theme-bg2 text-white f-14 f-w-400 float-right">100%</label>
                                     </div>
                                 </div>
-                                <h2 className="mt-2 f-w-300">45<sub className="text-muted f-14">Competitors</sub></h2>
-                                <h6 className="text-muted mt-3 mb-0">You can participate in event </h6>
+                                <h2 className="mt-2 f-w-300">Only 1 <sub className="text-muted f-14">Winner</sub></h2>
+                                <h6 className="text-muted mt-3 mb-0">Play your role and vote </h6>
                                 <i className="fa fa-angellist text-c-purple f-50"/>
                             </Card.Body>
                         </Card>
